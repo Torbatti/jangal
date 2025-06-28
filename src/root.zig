5009abd6eab41f26a240f3ff2595eb703ff7a142
torbatti <torbattii@gmail.com>
2025-06-28 12:46:48 +0330

    add: endian/compress and mincraft enums

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,22 @@
 pub const tardy = @import("tardy");
 
 pub const nbt = @import("nbt/root.zig");
+
+pub const MinecraftEdition = enum(u8) {
+    none = 0,
+    java = 1,
+    bedrock = 2,
+    pocket = 3, // It was replaced by Minecraft Bedrock on November 18, 2016
+    classic = 4,
+};
+
+pub const CompressKind = enum(u8) {
+    none = 0,
+    gzip = 1,
+    zlip = 2,
+};
+
+pub const EndianOrder = enum(u2) {
+    big_endian = 0,
+    short_endian = 1,
+};
