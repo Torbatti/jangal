5293cf9b47838d0702cca9ddc1a161f6856f1043
torbatti <torbattii@gmail.com>
2025-06-28 20:36:07 +0330

    java init

--- src/root.zig
+++ src/root.zig
@@ -3,6 +3,8 @@ pub const jaio = @import("jaio/root.zig");
 
 pub const nbt = @import("nbt/root.zig");
 
+pub const java = @import("java/root.zig");
+
 pub const MinecraftEdition = enum(u8) {
     none = 0,
     java = 1,
@@ -10,6 +12,8 @@ pub const MinecraftEdition = enum(u8) {
     pocket = 3, // It was replaced by Minecraft Bedrock on November 18, 2016
     classic = 4,
 };
+
+pub const JavaProtocolVersion = java.ProtocolVersion;
 
 pub const CompressKind = enum(u8) {
     none = 0,
